6104e08ac49e0ac0f936118f09315f10ea04b407
phillip.ko38 <phillipko38@outlook.com>
2017-06-03 21:37:27 +0800

    added nunit and newtonsoft json

--- HelloJenkins/Program.cs
+++ HelloJenkins/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 
 namespace HelloJenkins
 {
@@ -11,7 +13,27 @@ namespace HelloJenkins
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello Jenkins! Hello World");
+            JArray array = new JArray();
+            array.Add("Manual text");
+            array.Add(new DateTime(2000, 5, 23));
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
             Console.ReadLine();
+        }
+
+        public static bool Test()
+        {
+            return true;
+        }
+    }
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestFunc()
+        {
+            Assert.AreEqual(Program.Test(), true);
         }
     }
 }
