a1f60a42339b26d1ec68ae2cf661db07c207d31a
phillip.ko38 <phillipko38@outlook.com>
2017-06-04 18:33:00 +0800

    passed

--- HelloJenkins/Program.cs
+++ HelloJenkins/Program.cs
@@ -12,6 +12,7 @@ namespace HelloJenkins
     {
         public static void Main(string[] args)
         {
+			int x;
             Console.WriteLine("Hello Jenkins!");
             JArray array = new JArray();
             array.Add("Manual text");
